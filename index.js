5b137d51722720ff64a5a7bde28fae348c806c4b
Ridwan Adebosin <adebosinadewale@gmail.com>
2023-11-08 12:51:19 +0100

    Funtionality added

---
+++ index.js
@@ -0,0 +1,46 @@
+const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const stopButtonEl = document.getElementById("stop");
+const resetButtonEl = document.getElementById("reset");
+
+let startTime = 0;
+let elapsedTime = 0;
+//calculate intervals
+let timerInterval;
+
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timerEl.textContent = formatTime(elapsedTime);
+  }, 10);
+}
+
+function formatTime(elapsedTime) {
+  //to show the miliseconds
+  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+
+  return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    "." +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+    "." +
+    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+  );
+}
+function stopTimer() {
+  console.log("stop");
+}
+function resetTimer() {
+  stop("");
+}
+
+startButtonEl.addEventListener("click", startTimer);
+stopButtonEl.addEventListener("click", stopTimer);
+resetButtonEl.addEventListener("click", resetTimer);
